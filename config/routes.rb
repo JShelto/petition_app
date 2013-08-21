63ef4af7af8c2735fdea5c5ccd6c6fc53c1b39cc
JShelto <jshelto@sas.upenn.edu>
2013-08-21 10:16:42 -0700

    users part way done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PetitionApp::Application.routes.draw do
+  devise_for :users
 
-  root :to => "home#index"
+  root :to => "devise/sessions#new"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+
 ActiveRecord::Schema.define(:version => 20130821171800) do
 
   create_table "petitions", :force => true do |t|
