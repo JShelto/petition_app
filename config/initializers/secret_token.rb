49a40d4015dd23a3ba5d50f14f2df8226b27660b
JShelto <jshelto@sas.upenn.edu>
2013-08-21 09:55:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetitionApp::Application.config.secret_token = '6c0a1eb109a1016887f7e14510dab01905df8fe53fd5c2a368cb748ed136b9c497cdac085671d91efed19b8b88c8906562f85f5c63832b86adaf7ce9a45fd3a7'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+PetitionApp::Application.routes.draw do
+
+  root :to => "home#index"
+
+end
